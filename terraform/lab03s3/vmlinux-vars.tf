a80a9922d0975e85d467f390d6e0653b5ce88ec4
ken-cogniac <ken@cogniac.co>
2023-01-31 14:51:14 -0500

    lab03- added outputs.tf and lifecycle

---
+++ terraform/lab03s3/vmlinux-vars.tf
@@ -0,0 +1,51 @@
+locals {
+  common_tags = {
+    Name  = "Terraform-Class"
+    Project = "Learning"
+    ContactEmail = "n01538887@humbermail.ca"
+    Environment = "Lab"
+  }
+}
+variable "linux_name" {
+  default = "n01538887-u-vm1"
+}
+
+variable "size" {
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  default = "n01538887"
+}
+
+variable "public_key" {
+  default = "~/.ssh/automation_rsa.pub"
+}
+
+variable "os_disk_attributes" {
+  type = object({
+    storage_account_type = string
+    disk_size = number
+    caching = string
+  })
+  default = {
+    storage_account_type = "Premium_LRS"
+    disk_size = 32
+    caching = "ReadWrite"
+  }
+}
+
+variable "ubuntu_linux_os_info" {
+  type = object({
+    publisher = string
+    offer = string
+    sku = string
+    version = string
+  })
+  default = {
+    publisher = "Canonical"
+    offer = "UbuntuServer"
+    sku = "19.04"
+    version = "latest"
+  }
+}
